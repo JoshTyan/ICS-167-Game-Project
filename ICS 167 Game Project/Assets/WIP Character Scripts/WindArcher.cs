f57bd0f2a3240493c4320581a6d2661383265a7e
JoshTyan <tyanj@uci.edu>
2023-02-07 19:38:42 -0800

    WIP Character Script Folder Made
    WIP Character Scrip Folder, Character Script Folder is empty.

---
+++ ICS 167 Game Project/Assets/WIP Character Scripts/WindArcher.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArcher : CharacterScript
+{
+    private int maxHealth = 20;
+    private int damage = 2;
+}
