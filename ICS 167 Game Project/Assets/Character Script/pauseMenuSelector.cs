209a5f1dffbcada96c5d020b8d33f519642550a6
Shoen1KR <krogan1@uci.edu>
2023-02-08 16:30:02 -0800

    Game Pause Menu finished

---
+++ ICS 167 Game Project/Assets/Character Script/pauseMenuSelector.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenuSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool gameIsPause = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(gameIsPause){
+                resume();
+            }
+            else{
+                pause();
+            }
+        }
+    }
+    public void resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPause=false;
+    }
+
+    private void pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPause=true;
+    }
+
+    public void goToMenu(){
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+}
