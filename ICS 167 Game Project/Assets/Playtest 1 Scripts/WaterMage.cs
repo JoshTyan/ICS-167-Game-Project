53590416c759e9200a166275b1bf5f50f1f1974a
JoshTyan <tyanj@uci.edu>
2023-02-08 19:00:18 -0800

    Added Health Potion
    Added Health Potion Prefab, Script.
    Updated character class scripts to heal hp.

--- ICS 167 Game Project/Assets/Playtest 1 Scripts/WaterMage.cs
+++ ICS 167 Game Project/Assets/Playtest 1 Scripts/WaterMage.cs
@@ -6,10 +6,27 @@ using UnityEngine;
 public class WaterMage : BaseHealth
 {
     //gets the corresponding base hp for the character
+    public float maxHealth;
     public float health;
     public WaterMage() : base()
     {
-        health = getMageHP();
+        maxHealth = getMageHP();
+    }
+
+    //set health equal to max health at start
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    //heal on health potion pickup
+    public void HealHealth(float healAmount)
+    {
+        health += healAmount;
+        if(health > maxHealth)
+        {
+            health = maxHealth;
+        }
     }
 
     public void TakeDamage(float damageAmount)
