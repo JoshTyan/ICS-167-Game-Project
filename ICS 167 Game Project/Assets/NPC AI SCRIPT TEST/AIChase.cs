49efa9e25c104a3be5b8675e12b1f75d80b25491
Shoen1KR <krogan1@uci.edu>
2023-03-19 23:58:04 -0700

    Finished

--- ICS 167 Game Project/Assets/NPC AI SCRIPT TEST/AIChase.cs
+++ ICS 167 Game Project/Assets/NPC AI SCRIPT TEST/AIChase.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Joshua Tyan, Kevin Rogan
+
 public class AIChase : MonoBehaviour
 {
     public GameObject player;
